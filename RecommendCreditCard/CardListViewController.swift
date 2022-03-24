7913ea178d03eac3aa9523ec3ec900e6059de2a7
Mac <zoa0945@gmail.com>
2022-03-24 18:23:43 +0900

    refactor: edit CreditCardDummy data

--- RecommendCreditCard/CardListViewController.swift
+++ RecommendCreditCard/CardListViewController.swift
@@ -11,7 +11,7 @@ import FirebaseDatabase
 import FirebaseFirestore
 
 class CardListViewController: UITableViewController {
-//    var ref: DatabaseReference! // Firebase Realtime Database
+    var ref: DatabaseReference! // Firebase Realtime Database
     let db = Firestore.firestore()
     var creditCardList: [CreditCard] = []
     
@@ -23,46 +23,46 @@ class CardListViewController: UITableViewController {
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
         
         // 실시간 데이터베이스 (Realtime Database) 읽기
-//        ref = Database.database().reference()
-//
-//        ref.observe(.value) { snapshot in
-//            guard let value = snapshot.value as? [String: [String: Any]] else { return }
-//
-//            do {
-//                let jsonData = try JSONSerialization.data(withJSONObject: value)
-//                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
-//                let cardList = Array(cardData.values)
-//                self.creditCardList = cardList.sorted { $0.rank < $1.rank }
-//
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                }
-//            } catch let error {
-//                print("ERROR JSON PARSING \(error.localizedDescription)")
-//            }
-//        }
-        
-        // Firestore Database 읽기
-        db.collection("creditCardList").addSnapshotListener { snapshot, error in
-            guard let document = snapshot?.documents else {
-                print("ERROR Firestore fetching document \(String(describing: error))")
-                return
-            }
-            self.creditCardList = document.compactMap({ doc -> CreditCard? in
-                do {
-                    let jsonData = try JSONSerialization.data(withJSONObject: doc.data(), options: [])
-                    let creditCard = try JSONDecoder().decode(CreditCard.self, from: jsonData)
-                    return creditCard
-                } catch let error {
-                    print("ERROR JSON PARSING \(error.localizedDescription)")
-                    return nil
+        ref = Database.database().reference()
+
+        ref.observe(.value) { snapshot in
+            guard let value = snapshot.value as? [String: [String: Any]] else { return }
+
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(cardData.values)
+                self.creditCardList = cardList.sorted { $0.rank < $1.rank }
+
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
-            }).sorted { $0.rank < $1.rank }
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
+            } catch let error {
+                print("ERROR JSON PARSING \(error.localizedDescription)")
             }
         }
+        
+        // Firestore Database 읽기
+//        db.collection("creditCardList").addSnapshotListener { snapshot, error in
+//            guard let document = snapshot?.documents else {
+//                print("ERROR Firestore fetching document \(String(describing: error))")
+//                return
+//            }
+//            self.creditCardList = document.compactMap({ doc -> CreditCard? in
+//                do {
+//                    let jsonData = try JSONSerialization.data(withJSONObject: doc.data(), options: [])
+//                    let creditCard = try JSONDecoder().decode(CreditCard.self, from: jsonData)
+//                    return creditCard
+//                } catch let error {
+//                    print("ERROR JSON PARSING \(error.localizedDescription)")
+//                    return nil
+//                }
+//            }).sorted { $0.rank < $1.rank }
+//
+//            DispatchQueue.main.async {
+//                self.tableView.reloadData()
+//            }
+//        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -95,33 +95,33 @@ class CardListViewController: UITableViewController {
         
         // 실시간 데이터베이스 (Realtime Database) 쓰기
         // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 있을 때
-//        let cardId = creditCardList[indexPath.row].id
-//        ref.child("Item\(cardId)/isSelected").setValue(true)
+        let cardId = creditCardList[indexPath.row].id
+        ref.child("Item\(cardId)/isSelected").setValue(true)
         
         // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 없을 때
-//        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardId).observe(.value) {
-//            [weak self] snapshot in
-//            guard let self = self,
-//                  let value = snapshot.value as? [String: [String: Any]],
-//                  let key = value.keys.first else { return }
-//
-//            self.ref.child("\(key)/isSelected").setValue(true)
-//        }
+        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardId).observe(.value) {
+            [weak self] snapshot in
+            guard let self = self,
+                  let value = snapshot.value as? [String: [String: Any]],
+                  let key = value.keys.first else { return }
+
+            self.ref.child("\(key)/isSelected").setValue(true)
+        }
         
         // Firestore Database 쓰기
-        // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 있을 때
-        let cardId = creditCardList[indexPath.row].id
-//        db.collection("creditCardList").document("card\(cardId)").updateData(["isSelected": true])
-        
-        // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 없을 때
-        db.collection("creditCardList").whereField("id", isEqualTo: cardId).getDocuments { snapshot, error in
-            guard let document = snapshot?.documents.first else {
-                print("ERROR fetching document")
-                return
-            }
-            
-            document.reference.updateData(["isSelected": true])
-        }
+//        // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 있을 때
+//        let cardId = creditCardList[indexPath.row].id
+////        db.collection("creditCardList").document("card\(cardId)").updateData(["isSelected": true])
+//
+//        // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 없을 때
+//        db.collection("creditCardList").whereField("id", isEqualTo: cardId).getDocuments { snapshot, error in
+//            guard let document = snapshot?.documents.first else {
+//                print("ERROR fetching document")
+//                return
+//            }
+//
+//            document.reference.updateData(["isSelected": true])
+//        }
     }
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -133,30 +133,30 @@ class CardListViewController: UITableViewController {
             
             // 실시간 데이터베이스 (Realtime Database) 삭제
             // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 있을 때
-//            let cardId = creditCardList[indexPath.row].id
-//            ref.child("Item\(cardId)").removeValue()
+            let cardId = creditCardList[indexPath.row].id
+            ref.child("Item\(cardId)").removeValue()
             
             // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 없을 때
-//            ref.queryOrdered(byChild: "id").queryEqual(toValue: cardId).observe(.value) { [weak self] snapshot in
-//                guard let self = self,
-//                      let value = snapshot.value as? [String: [String: Any]],
-//                      let key = value.keys.first else { return }
-//                self.ref.child(key).removeValue()
-//            }
+            ref.queryOrdered(byChild: "id").queryEqual(toValue: cardId).observe(.value) { [weak self] snapshot in
+                guard let self = self,
+                      let value = snapshot.value as? [String: [String: Any]],
+                      let key = value.keys.first else { return }
+                self.ref.child(key).removeValue()
+            }
             
             // Firestore Database 삭제
             // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 있을 때
-            let cardId = creditCardList[indexPath.row].id
-//            db.collection("creditCardList").document("card\(cardId)").delete()
-            
-            // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 없을 때
-            db.collection("creditCardList").whereField("id", isEqualTo: cardId).getDocuments{ snapshot, error in
-                guard let document = snapshot?.documents.first else {
-                    print("ERROR fetching document")
-                    return
-                }
-                document.reference.delete()
-            }
+//            let cardId = creditCardList[indexPath.row].id
+////            db.collection("creditCardList").document("card\(cardId)").delete()
+//
+//            // 데이터베이스 객체의 key값(여기서는 Item0, 1, 2, ...)을 알 수 없을 때
+//            db.collection("creditCardList").whereField("id", isEqualTo: cardId).getDocuments{ snapshot, error in
+//                guard let document = snapshot?.documents.first else {
+//                    print("ERROR fetching document")
+//                    return
+//                }
+//                document.reference.delete()
+//            }
         }
     }
 }
